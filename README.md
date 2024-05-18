1810ee6264be206ac3c614cad0fa457f99446d60
noaJ4Q <noe.jaraq@gmail.com>
2024-05-18 01:38:06 -0500

    adding jwt

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
-# Install and execute Flask
+# Install and execute Slice Manager
 
 ## Steps
 
-1. Install flask
+1. Install libraries
+
+```sh
+pip install flask # To create server
+pip install pyjwt # To use JWI
 
 ```
-pip install flask
 
-```
+2. Run flask server
 
-2. Run server
-
-```
+```sh
 python main.py
 
 ```
 
-3. Run server debug mode
+3. Run flask server debug mode
 
-```
+```sh
 flask --app main.py --debug run
 ```
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
+import jwt
 
 app = Flask(__name__)
 
 @app.route('/auth', methods=['POST'])
 def auth_user():
+
   # Get the username and password from JSON request
   username = request.json['username']
   password = request.json['password']
+
   # Check if the username and password are not empty
   if username and password:
-    return jsonify({'message': 'User authenticated successfully'})
+    if valid_user(username, password):
+      token = jwt.encode({'username': username}, 'secret', algorithm='HS256')
+      return jsonify({'message': 'success', 'token': token})
   else:
     return jsonify({'message': 'Username and password are required'})
+
+def valid_user(username, password):
+  return True
 
 if __name__=='__main__':
   app.run()
