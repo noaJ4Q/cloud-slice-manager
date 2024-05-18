fa6dd88a45755f3cc529c8d06002550997f35e18
noaJ4Q <noe.jaraq@gmail.com>
2024-05-17 19:57:05 -0500

    advance

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ pip install flask
 2. Run server
 
 ```
-python server.py
+python main.py
 
+```
+
+3. Run server debug mode
+
+```
+flask --app main.py --debug run
 ```
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/auth')
-def hello_world():
-  return '<p>Hello world</p>'
+@app.route('/auth', methods=['POST'])
+def auth_user():
+  # Get the username and password from JSON request
+  username = request.json['username']
+  password = request.json['password']
+  # Check if the username and password are not empty
+  if username and password:
+    return jsonify({'message': 'User authenticated successfully'})
+  else:
+    return jsonify({'message': 'Username and password are required'})
 
 if __name__=='__main__':
   app.run()
