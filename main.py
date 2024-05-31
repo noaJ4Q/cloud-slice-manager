46d3836b0449da9dba792fc0e00675d89269b04d
smavend <amanrique068@gmail.com>
2024-05-31 09:27:35 -0500

    update for a free port

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from modules.crudModule import crudModule
 from modules.authModule import authModule
+from modules.crudModule import crudModule
 
 app = Flask(__name__)
 app.register_blueprint(crudModule)
 app.register_blueprint(authModule)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8080)
