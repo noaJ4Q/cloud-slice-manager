1c50c76444e9542158203d97e9669ae9abbe2fae
smavend <amanrique068@gmail.com>
2024-05-31 21:22:52 -0500

    better style

--- modules/crudModule.py
+++ modules/crudModule.py
@@ -112,7 +112,7 @@ def generate_diag(userId, json_data):
                     color=edge_info.get("color", ""),
                 )
 
-    net.repulsion(node_distance=300, spring_length=150)
+    net.repulsion(node_distance=300)
 
     html_file = f"topology_graph/{userId}.html"
     net.show(html_file)
