b1f2761c3dfb3a11ed8909b9f50bada292445592
smavend <amanrique068@gmail.com>
2024-06-09 13:00:53 -0500

    adding logs to client output

--- modules/openStack/openStackModule.py
+++ modules/openStack/openStackModule.py
@@ -40,48 +40,51 @@ ADMIN_PROJECT_NAME = "admin"
 
 def main(json_data):
     log_info(logger, "Inicio de la operación de OpenStack")
-
-    # ===================================================== PROJECT CREATION =====================================================
-    command = (
-        "openstack project create --description "
-        + json_data["deployment"]["details"]["project_description"]
-        + " "
-        + json_data["deployment"]["details"]["project_name"]
-    )
-
-    success, output = execute_bash_command(command)
+    # ===================================================== INITIALIZATION =====================================================
+    success, output = execute_bash_command(". ~/env-scripts/admin-openrc")
     if success:
-        log_info(logger, "PROJECT CREATED SUCCESSFULLY")
-        command2 = (
-            "openstack role add admin --project "
+        # ===================================================== PROJECT CREATION =====================================================
+        command = (
+            "openstack project create --description "
+            + json_data["deployment"]["details"]["project_description"]
+            + " "
             + json_data["deployment"]["details"]["project_name"]
-            + " --user admin"
         )
-        success, output = execute_bash_command(command2)
-        if success:
-            log_info(logger, "ROLE ADDED SUCCESSFULLY")
-            # ===================================================== TOKEN FOR ADMIN USER =====================================================
-            resp1 = password_authentication_with_scoped_authorization(
-                KEYSTONE_ENDPOINT,
-                ADMIN_USER_DOMAIN_NAME,
-                ADMIN_USER_USERNAME,
-                ADMIN_USER_PASSWORD,
-                DOMAIN_ID,
-                ADMIN_PROJECT_NAME,
-            )
-            if resp1.status_code == 201:
-                log_info(logger, "SUCCESSFUL ADMIN AUTHENTICATION")
-                admin_token = resp1.headers["X-Subject-Token"]
-                logs = admin_token_for_project(
-                    admin_token=admin_token, json_data=json_data
-                )
-                log_info(logger, logs)
-            else:
-                log_error(logger, "FAILED ADMIN AUTHENTICATION")
-        else:
-            log_error(logger, "FAILED ROLE ADDITION")
-    else:
-        log_error(logger, "FAILED PROJECT CREATION")
 
+        success, output = execute_bash_command(command)
+        if success:
+            log_info(logger, "PROJECT CREATED SUCCESSFULLY")
+            command2 = (
+                "openstack role add admin --project "
+                + json_data["deployment"]["details"]["project_name"]
+                + " --user admin"
+            )
+            success, output = execute_bash_command(command2)
+            if success:
+                log_info(logger, "ROLE ADDED SUCCESSFULLY")
+                # ===================================================== TOKEN FOR ADMIN USER =====================================================
+                resp1 = password_authentication_with_scoped_authorization(
+                    KEYSTONE_ENDPOINT,
+                    ADMIN_USER_DOMAIN_NAME,
+                    ADMIN_USER_USERNAME,
+                    ADMIN_USER_PASSWORD,
+                    DOMAIN_ID,
+                    ADMIN_PROJECT_NAME,
+                )
+                if resp1.status_code == 201:
+                    log_info(logger, "SUCCESSFUL ADMIN AUTHENTICATION")
+                    admin_token = resp1.headers["X-Subject-Token"]
+                    logs = admin_token_for_project(
+                        admin_token=admin_token, json_data=json_data
+                    )
+                    log_info(logger, logs)
+                else:
+                    log_error(logger, "FAILED ADMIN AUTHENTICATION")
+            else:
+                log_error(logger, "FAILED ROLE ADDITION")
+        else:
+            log_error(logger, "FAILED PROJECT CREATION")
+    else:
+        log_error(logger, "FAILED INITIALIZATION")
     log_contents = log_buffer.getvalue()
     return log_contents
